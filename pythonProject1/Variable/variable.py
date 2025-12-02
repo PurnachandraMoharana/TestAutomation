5d1fac0045077cbf5ca17796049767a11425873b
PurnachandraMoharana <kkrishnaengg@gmail.com>
2025-12-02 17:45:02 +0530

    "adding my 1st code to git"

---
+++ pythonProject1/Variable/variable.py
@@ -0,0 +1,53 @@
+a=10
+'''
+a is variable 
+= is a assignment operator
+10 is a value which is assign to a variable 
+'''
+print(a)
+
+print(a,id(a))
+
+
+k=50
+l=50
+
+
+print(k,id(k))
+print(l, id(l))
+
+
+
+x = y = z = 100
+print("value of x is :", x)
+print("value of y is :", y)
+print("value of z is :", z)
+
+
+
+
+
+
+
+
+var = 10
+var1 = 20
+
+
+
+print("addition value is  ", var + var1)
+print("substaction", var - var1)
+print("multipication ", var * var1)
+print("Division", var / var1)
+print("Division with double", var // var1)
+print("remender ", var % var1)
+print("eqals to  ", var == var1)
+print("not eqals to   ", var != var1)
+print("power   ", var ** var1)
+
+# output = var1 + var
+#
+# print("output :", output)
+
+
+
